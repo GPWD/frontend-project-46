b92397bc626d62ca3bfd772872d47abda089a026
georgy <palatckingeogy@yandex.ru>
2024-03-24 13:42:28 +0300

    Added logic comparing two json files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import getParse from './parse.js';
 import getReadFile from './readfile.js';
+import _ from 'lodash';
 
 
 const genDiff = (filepath1, filepath2, format) => {
-	const file1 = getReadFile(filepath1);
-	const file2 = getReadFile(filepath2);
+	const obj1 = getParse(getReadFile(filepath1));
+	const obj2 = getParse(getReadFile(filepath2));
 
-	return getParse(file1);
+	const sortedKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
+	let resultStr = '';
+
+	for (const key of sortedKeys) {
+		if (!obj2.hasOwnProperty(key)) {
+			resultStr += `\n  - ${key}: ${obj1[key]}`
+		} else if (!obj1.hasOwnProperty(key)) {
+		  resultStr += `\n  + ${key}: ${obj2[key]}`
+		}
+
+		if (obj1.hasOwnProperty(key) === obj2.hasOwnProperty(key)) {
+			if (obj1[key] === obj2[key]) {
+				resultStr += `\n    ${key}: ${obj1[key]}`
+			} else {
+				resultStr += `\n  - ${key}: ${obj1[key]}`
+				resultStr += `\n  + ${key}: ${obj2[key]}`
+			}
+		}
+  }
+
+	return `{ ${resultStr} \n}`;
 };
 
 export default genDiff;
