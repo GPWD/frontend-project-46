22530621d71d46a2c09e95900721547f843ca099
georgy <palatckingeogy@yandex.ru>
2024-03-26 16:36:06 +0300

    One condition removed

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
+import _ from 'lodash';
 import getParse from './parse.js';
 import getReadFile from './readfile.js';
-import _ from 'lodash';
 
+const genDiff = (filePath1, filePath2) => {
+  const obj1 = getParse(getReadFile(filePath1));
+  const obj2 = getParse(getReadFile(filePath2));
 
-const genDiff = (filepath1, filepath2, format) => {
-	const obj1 = getParse(getReadFile(filepath1));
-	const obj2 = getParse(getReadFile(filepath2));
+  const sortedKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
+  let resultStr = '';
 
-	const sortedKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-	let resultStr = '';
-
-	for (const key of sortedKeys) {
-		if (!obj2.hasOwnProperty(key)) {
-			resultStr += `\n  - ${key}: ${obj1[key]}`
-		} else if (!obj1.hasOwnProperty(key)) {
-		  resultStr += `\n  + ${key}: ${obj2[key]}`
-		}
-
-		if (obj1.hasOwnProperty(key) === obj2.hasOwnProperty(key)) {
-			if (obj1[key] === obj2[key]) {
-				resultStr += `\n    ${key}: ${obj1[key]}`
-			} else {
-				resultStr += `\n  - ${key}: ${obj1[key]}`
-				resultStr += `\n  + ${key}: ${obj2[key]}`
-			}
-		}
+  for (const key of sortedKeys) {
+    if (!Object.hasOwnProperty.call(obj2, key)) {
+      resultStr += `\n  - ${key}: ${obj1[key]}`;
+    } else if (!Object.hasOwnProperty.call(obj1, key)) {
+      resultStr += `\n  + ${key}: ${obj2[key]}`;
+    } else if (Object.hasOwnProperty.call(obj1, key) === Object.hasOwnProperty.call(obj2, key)) {
+      if (obj1[key] === obj2[key]) {
+        resultStr += `\n    ${key}: ${obj1[key]}`;
+      } else {
+        resultStr += `\n  - ${key}: ${obj1[key]}`;
+        resultStr += `\n  + ${key}: ${obj2[key]}`;
+      }
+    }
   }
 
-	return `{ ${resultStr} \n}`;
+  return `{ ${resultStr} \n}`;
 };
 
 export default genDiff;
