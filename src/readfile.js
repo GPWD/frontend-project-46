1850a2acc095ab430062ffdc3f704768f2a36cd5
georgy <palatckingeogy@yandex.ru>
2024-03-26 18:13:10 +0300

    Bugs fix

--- src/readfile.js
+++ src/readfile.js
@@ -2,12 +2,12 @@ import path from 'node:path';
 import fs from 'node:fs';
 
 const getReadFile = (filePath) => {
-	if (filePath.startsWith('/')) {
-		return fs.readFileSync(filePath);
-	}
+  if (filePath.startsWith('/')) {
+    return fs.readFileSync(filePath);
+  }
 
-	const absoluteFilePath = path.resolve(process.cwd(), filePath);
-	return fs.readFileSync(absoluteFilePath);
+  const absoluteFilePath = path.resolve(process.cwd(), filePath);
+  return fs.readFileSync(absoluteFilePath);
 };
 
 export default getReadFile;
