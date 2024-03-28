8cae152b4f05d31aa483766fde54c63e572cf76d
georgy <palatckingeogy@yandex.ru>
2024-03-28 12:46:03 +0300

    Bugs fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
-import genDiff from '../src/index.js';
 import { Command } from 'commander';
-const program = new Command();
+import genDiff from '../src/index.js';
 
+const program = new Command();
 program
-	.name('gendiff')
-	.description('Compares two configuration files and shows a difference.')
-	.version('1.0.0')
-	.option('-f, --format [type]', 'output format')
-	.arguments('<filepath1>')
-	.arguments('<filepath2>')
-	.action((filepath1, filepath2) => {
-		console.log(genDiff(filepath1, filepath2))
-	})
-	.parse();
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1>')
+  .arguments('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  })
+  .parse();
