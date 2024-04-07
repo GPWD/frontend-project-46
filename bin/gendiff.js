12994296688122f3f99dd65d38c4ff94b348aef0
georgy <palatckingeogy@yandex.ru>
2024-04-07 20:31:51 +0300

    Bug fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,11 +6,11 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-	.version('1.0.0')
-	.option('-f, --format [type]', 'output format')
-	.arguments('<filepath1>')
-	.arguments('<filepath2>')
-	.action((filepath1, filepath2) => {
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1>')
+  .arguments('<filepath2>')
+  .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
   })
   .parse();
