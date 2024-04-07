80160a3d2d2020c27f2c87d6afc8bd9e21bdfeb7
georgy <palatckingeogy@yandex.ru>
2024-04-07 20:35:07 +0300

    Removed cycle and changed conditions

--- src/index.js
+++ src/index.js
@@ -3,29 +3,26 @@ import getParse from './parsefile.js';
 import getReadFile from './readfile.js';
 
 const genDiff = (filePath1, filePath2) => {
-		const readFileOne = getReadFile(filePath1);
-		const readFileTwo = getReadFile(filePath2);
-	
-		const obj1 = getParse(filePath1, readFileOne);
-		const obj2 = getParse(filePath2, readFileTwo);
-	
-		const sortedKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-		let resultStr = '';
-	
-		for (const key of sortedKeys) {
-			if (!Object.hasOwn(obj2, key)) {
-				resultStr += `\n  - ${key}: ${obj1[key]}`;
-			} else if (!Object.hasOwn(obj1, key)) {
-				resultStr += `\n  + ${key}: ${obj2[key]}`;
-			} else if (Object.hasOwn(obj1, key) === Object.hasOwn(obj2, key) && obj1[key] === obj2[key]) {
-				resultStr += `\n    ${key}: ${obj1[key]}`;
-			} else {
-				resultStr += `\n  - ${key}: ${obj1[key]}`;
-				resultStr += `\n  + ${key}: ${obj2[key]}`;
-			}
-		}
-	
-	return `{${resultStr}\n}`;
+  const parsedData1 = getParse(filePath1, getReadFile(filePath1));
+  const parsedData2 = getParse(filePath2, getReadFile(filePath2));
+
+  const keys = _.sortBy(_.union(Object.keys(parsedData1), Object.keys(parsedData2)));
+
+  const listData = keys.map((key) => {
+    if (!Object.hasOwn(parsedData2, key)) {
+      return `  - ${key}: ${parsedData1[key]}`;
+    }
+    if (!Object.hasOwn(parsedData1, key)) {
+      return `  + ${key}: ${parsedData2[key]}`;
+    }
+    if (parsedData1[key] === parsedData2[key]) {
+      return `    ${key}: ${parsedData1[key]}`;
+    }
+    return `  - ${key}: ${parsedData1[key]}\n  + ${key}: ${parsedData2[key]}`;
+  });
+
+  const strData = listData.join('\n');
+  return `{\n${strData}\n}`;
 };
 
 export default genDiff;
