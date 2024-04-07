5055d6017786a769c0d9e399c61ace62a57926e9
georgy <palatckingeogy@yandex.ru>
2024-04-07 17:29:58 +0300

    Added logick read file

---
+++ src/readfile.js
@@ -0,0 +1,13 @@
+import path from 'node:path';
+import fs from 'node:fs';
+
+const getReadFile = (filePath) => {
+  if (filePath.startsWith('/')) {
+    return fs.readFileSync(filePath);
+  }
+
+  const absoluteFilePath = path.resolve(process.cwd(), filePath);
+  return fs.readFileSync(absoluteFilePath);
+};
+
+export default getReadFile;
