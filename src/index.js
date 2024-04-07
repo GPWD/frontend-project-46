45493bee7601325aecbeac6a6ecbe9fef7cc310a
georgy <palatckingeogy@yandex.ru>
2024-04-07 17:32:02 +0300

    Added main logick

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const genDiff = () => {
+import getParse from './parsefile.js';
+import getReadFile from './readfile.js';
 
+const genDiff = (filePath1, filePath2) => {
+	return getParse(filePath1, getReadFile(filePath1));
 };
+
 export default genDiff;
