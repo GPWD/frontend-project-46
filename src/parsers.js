e5f09bdee54caf573564ffac75d680f1be171b85
georgy <palatckingeogy@yandex.ru>
2024-04-11 23:22:05 +0300

    Added parse yaml file

---
+++ src/parsers.js
@@ -0,0 +1,17 @@
+import path from 'node:path';
+import yaml from 'js-yaml';
+import getReadFile from './readfile.js';
+
+const getParse = (filePath, file) => {
+  const formatFile = path.extname(filePath);
+  if (formatFile === '.json') {
+    return JSON.parse(file);
+  }
+
+  if (formatFile === '.yml' || formatFile === '.yaml') {
+    return yaml.load(getReadFile(filePath));
+  }
+  return file;
+};
+
+export default getParse;
