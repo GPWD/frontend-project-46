39f151219a1dca5a65b36c8184e254f8ad919b1c
georgy <palatckingeogy@yandex.ru>
2024-04-30 14:47:01 +0300

    Added default formater.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import genDiff from '../src/index.js';
+import diff from '../src/index.js';
 
 const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --formater [type]', 'output format', 'stylish')
   .arguments('<filepath1>')
   .arguments('<filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    console.log(diff(filepath1, filepath2));
   })
   .parse();
