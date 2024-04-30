cb88a095811434467f56ee934d0b8acd8d0b475e
georgy <palatckingeogy@yandex.ru>
2024-05-01 00:33:27 +0300

    A separate function has been created for generating objects

--- src/index.js
+++ src/index.js
@@ -3,36 +3,39 @@ import getParse from './parsers.js';
 import getReadFile from './readfile.js';
 import stylish from './formaters/stylish.js';
 
+const compareKeys = (key, obj1, obj2, iter) => {
+  if (!Object.hasOwnProperty.call(obj2, key)) {
+    return { key, type: 'deleted', value: obj1[key] };
+  }
+
+  if (!Object.hasOwnProperty.call(obj1, key)) {
+    return { key, type: 'added', value: obj2[key] };
+  }
+
+  if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+    return { key, type: 'nested', children: iter(obj1[key], obj2[key]) };
+  }
+
+  if (obj1[key] !== obj2[key]) {
+    return {
+      key,
+      type: 'changes',
+      value1: obj1[key],
+      value2: obj2[key],
+    };
+  }
+  return { key, type: 'unchanges', value: obj1[key] };
+};
+
 const diff = (filePath1, filePath2, formater = stylish) => {
   const parsedData1 = getParse(filePath1, getReadFile(filePath1));
   const parsedData2 = getParse(filePath2, getReadFile(filePath2));
 
   const iter = (obj1, obj2) => {
     const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-    const res = keys.map((key) => {
-      if (!Object.hasOwnProperty.call(obj2, key)) {
-        return { key, type: 'deleted', value: obj1[key] };
-      }
-      if (!Object.hasOwnProperty.call(obj1, key)) {
-        return { key, type: 'added', value: obj2[key] };
-      }
-      if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-        return { key, type: 'nested', children: iter(obj1[key], obj2[key]) };
-      }
-
-      if (obj1[key] !== obj2[key]) {
-        return {
-          key,
-          type: 'changes',
-          value1: obj1[key],
-          value2: obj2[key],
-        };
-      }
-      return { key, type: 'unchanges', value: obj1[key] };
-    });
-
-    return res;
+    return keys.map((key) => compareKeys(key, obj1, obj2, iter));
   };
+
   return formater(iter(parsedData1, parsedData2));
 };
 
