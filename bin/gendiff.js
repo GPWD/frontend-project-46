1c31b3ea6b86b05c6c49aa9cedd85def7a232d40
georgy <palatckingeogy@yandex.ru>
2024-05-06 00:29:28 +0300

    Added new option and rename name.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,13 +4,14 @@ import diff from '../src/index.js';
 
 const program = new Command();
 program
-  .name('gendiff')
+  .name('genDiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --formater [type]', 'output format', 'stylish')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1>')
   .arguments('<filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(diff(filepath1, filepath2));
+    const myFormat = program.opts().format;
+    console.log(diff(filepath1, filepath2, myFormat));
   })
   .parse();
