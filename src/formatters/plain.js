9257d668864893e1a122525a6aba2ccfb10a3a32
georgy <palatckingeogy@yandex.ru>
2024-05-06 16:44:32 +0300

    Changed the name of the constant

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -16,7 +16,7 @@ const stringify = (value) => {
 const plain = (data) => {
   const iter = (tree, parent) => {
     const filtredTree = tree.filter((node) => node.type !== 'unchanged');
-    const res = filtredTree.map((obj) => {
+    const lines = filtredTree.map((obj) => {
       const property = parent ? `${parent}.${obj.key}` : obj.key;
       switch (obj.type) {
         case 'added':
@@ -31,7 +31,7 @@ const plain = (data) => {
           throw new Error(`Type is not defined - ${obj.type}`);
       }
     });
-    return res.join('\n');
+    return lines.join('\n');
   };
   return iter(data, 0);
 };
